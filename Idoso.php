d2050a8f9c79cdc822d638eeb9ce9c9aed79085c
yuritx <texeira.yuri@hotmail.com>
2018-03-20 20:19:39 -0300

    inicial

---
+++ Idoso.php
@@ -0,0 +1,63 @@
+	
+	<?php
+		
+		class Idoso{
+			
+			private $idade;
+			private $nome;
+			private $medicamentos;
+			private $horarioMedicamento;
+			private $alimentos;
+			private $horarioAlimento;
+			private $datasConsultas;
+			private $alergias;
+			
+			
+			function Idoso($informacoes){
+			
+			}
+			
+			function getIdade(){
+				
+			}
+			
+			function getNome(){
+				
+			}
+			
+			function getMedicamentos(){
+				
+			}
+			
+			function getAlimentos(){
+				
+			}
+			function getConsultas(){
+				
+			}
+			function getAlergias(){
+				
+			}
+			
+			function getProximoMedicamento(){
+				
+			}
+			function getProximaConsulta(){
+				
+			}
+			
+			function tomarMedicamento($medicamento, $horario){
+				
+			}
+			
+			function refeicao($alimento, $horario){
+				
+			}
+			
+			function consultar($data){
+				
+			}
+			
+			
+		}
+	?>
